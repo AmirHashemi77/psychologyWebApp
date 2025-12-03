c4f128dbeb3ab0440c7e5f41db550d20c7da9c1b
AmirHashemi77 <amirhashemi539@gmail.com>
2025-12-04 02:38:51 +0330

    footer

--- src/component/common/layout/footer/Footer.tsx
+++ src/component/common/layout/footer/Footer.tsx
@@ -4,10 +4,12 @@ import { FaFacebook, FaInstagram, FaTelegram, FaWhatsapp } from "react-icons/fa"
 import FooterLogo from "./FooterLogo";
 import { FaLocationDot, FaPhoneFlip } from "react-icons/fa6";
 import { RiTelegram2Fill } from "react-icons/ri";
+import Image from "next/image";
 
 const Footer: FC = () => {
   return (
-    <footer className="w-full bg-primary p-5 mt-10">
+    <footer className="w-full bg-primary p-5 mt-10 relative ">
+      <Image src="/images/footer-bg.png" fill alt="bg-footer" className=" opacity-10" />
       <div className="w-full max-w-6xl mx-auto">
         <div className="flex flex-col gap-28 md:gap-10 md:flex-row w-full items-center justify-between flex-wrap">
           <Link className="font-extrabold text-5xl text-primary-foreground " href="/">
