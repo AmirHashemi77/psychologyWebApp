79c3eb7d0603c8a4704ab9d51d42cf9e4d88ce24
AmirHashemi77 <amirhashemi539@gmail.com>
2025-12-04 00:36:25 +0330

    fix footer bugs

--- src/component/common/layout/footer/Footer.tsx
+++ src/component/common/layout/footer/Footer.tsx
@@ -2,47 +2,64 @@ import Link from "next/link";
 import { FC } from "react";
 import { FaFacebook, FaInstagram, FaTelegram, FaWhatsapp } from "react-icons/fa";
 import FooterLogo from "./FooterLogo";
+import { FaLocationDot, FaPhoneFlip } from "react-icons/fa6";
+import { RiTelegram2Fill } from "react-icons/ri";
 
 const Footer: FC = () => {
   return (
     <footer className="w-full bg-primary p-5 mt-10">
-      <div className="w-full max-w-4xl mx-auto">
-        <div className="flex flex-col gap-28 md:gap-10 md:flex-row w-full items-center justify-between">
-          <div className="flex flex-col md:flex-row items-center gap-32">
-            <Link className="font-extrabold text-5xl text-primary-foreground" href="/">
-              <FooterLogo />
-            </Link>
-            <div className="flex flex-col gap-2">
-              <p className="text-primary-foreground text-3xl font-semibold font-vazir">دسترسی ها</p>
-              <Link className="text-primary-foreground text-sm lg:text-lg font-vazir hover:text-muted-foreground transition-all duration-200" href="">
+      <div className="w-full max-w-6xl mx-auto">
+        <div className="flex flex-col gap-28 md:gap-10 md:flex-row w-full items-center justify-between flex-wrap">
+          <Link className="font-extrabold text-5xl text-primary-foreground " href="/">
+            <FooterLogo />
+          </Link>
+          <div className="flex flex-row gap-10 md:flex-row w-full items-center md:items-start justify-between flex-wrap">
+            <div className="flex flex-col items-start gap-5">
+              <p className="text-primary-foreground text-3xl font-semibold font-vazir"> اطلاعات تماس</p>
+              <div className="flex items-start gap-4">
+                <FaLocationDot className="text-xl text-secondary" />
+                <span className="text-primary-foreground font-medium max-w-xs">تهران، میدان پونک، ضلع شمال شرقی میدان پونک، کوچه رمضانی</span>
+              </div>
+              <div className="flex items-start gap-4">
+                <FaPhoneFlip className="text-xl text-secondary" />
+                <span className="text-primary-foreground font-medium">۰۹۳۰۴۳۹۰۱۱۷</span>
+              </div>
+              <div className="flex items-start gap-4">
+                <RiTelegram2Fill className="text-xl text-secondary" />
+                <span className="text-primary-foreground font-medium max-w-xs">جهت رزرو وقت به شماره درج شده در تلگرام پیام دهید</span>
+              </div>
+            </div>
+            <div className="flex flex-col gap-5 ">
+              <p className="text-primary-foreground text-3xl font-semibold font-vazir">دســـترسی ها</p>
+              <Link className="text-primary-foreground  lg:text-lg font-vazir hover:text-muted-foreground transition-all duration-200" href="">
                 سوالات متداول
               </Link>
-              <Link className="text-primary-foreground text-sm lg:text-lg font-vazir hover:text-muted-foreground transition-all duration-200" href="">
+              <Link className="text-primary-foreground  lg:text-lg font-vazir hover:text-muted-foreground transition-all duration-200" href="">
                 نوبت دهی
               </Link>
-              <Link className="text-primary-foreground text-sm lg:text-lg font-vazir hover:text-muted-foreground transition-all duration-200" href="/blogs">
+              <Link className="text-primary-foreground  lg:text-lg font-vazir hover:text-muted-foreground transition-all duration-200" href="/blogs">
                 مقالات
               </Link>
-              <Link className="text-primary-foreground text-sm lg:text-lg font-vazir hover:text-muted-foreground transition-all duration-200" href="/aboutus">
+              <Link className="text-primary-foreground  lg:text-lg font-vazir hover:text-muted-foreground transition-all duration-200" href="/aboutus">
                 درباره ما
               </Link>
             </div>
-          </div>
-          <div className="flex flex-col items-start gap-5">
-            <p className="text-primary-foreground text-xl font-semibold font-vazir">ما را دنبال کنید :</p>
-            <div className="flex items-center gap-5">
-              <Link href="">
-                <FaFacebook className="text-3xl  text-secondary hover:text-primary-foreground transition-all duration-200" />
-              </Link>
-              <Link href="">
-                <FaInstagram className="text-3xl  text-secondary hover:text-primary-foreground transition-all duration-200" />
-              </Link>
-              <Link href="">
-                <FaWhatsapp className="text-3xl  text-secondary hover:text-primary-foreground transition-all duration-200" />
-              </Link>
-              <Link href="">
-                <FaTelegram className="text-3xl  text-secondary hover:text-primary-foreground transition-all duration-200" />
-              </Link>
+            <div className="flex flex-col items-start gap-5">
+              <p className="text-primary-foreground text-xl font-semibold font-vazir">ما را دنبال کنید :</p>
+              <div className="flex items-center gap-5">
+                <Link href="">
+                  <FaFacebook className="text-3xl  text-secondary hover:text-primary-foreground transition-all duration-200" />
+                </Link>
+                <Link href="">
+                  <FaInstagram className="text-3xl  text-secondary hover:text-primary-foreground transition-all duration-200" />
+                </Link>
+                <Link href="">
+                  <FaWhatsapp className="text-3xl  text-secondary hover:text-primary-foreground transition-all duration-200" />
+                </Link>
+                <Link href="">
+                  <FaTelegram className="text-3xl  text-secondary hover:text-primary-foreground transition-all duration-200" />
+                </Link>
+              </div>
             </div>
           </div>
         </div>
